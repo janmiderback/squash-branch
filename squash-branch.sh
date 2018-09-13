8fd562dec6f16bdea560e3fc0039ee1a667e5d38
Jan Miderbäck <jan.miderback@gmail.com>
2018-09-13 12:50:30 +0200

    README and base branch support (#1)
    * Update README.md
    
    * README on base branch support

--- squash-branch.sh
+++ squash-branch.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 # Squash a branch to its merge base, and commit using provided commit message.
+#
 # Usage:
-#   squash-branch [squash-branch] "commit-message"
-#   
+#   squash-branch [base-branch] [squash-branch] "commit-message"
+#
+# If base-branch is not provided, master is used.
+# If squash-branch is not provided, the current branch is used.
+# The squash-branch is always checked out before squashing it.
+#
+
 set -e
 
 if [[ $# -lt 1 || $# -gt 3 ]]; then
@@ -10,26 +16,30 @@ if [[ $# -lt 1 || $# -gt 3 ]]; then
     exit 1
 fi
 
-# TODO: Make it possible to provide base branch as a first argument
-#       before squash-branch.
-base_branch=master
-squash_branch=
-commit_message=
-
 if [[ $# -eq 1 ]]; then
+    base_branch=master
     squash_branch=$(git rev-parse --abbrev-ref HEAD)
     message=$1
 fi
 
 if [[ $# -eq 2 ]]; then
+    base_branch=master
     squash_branch=$1
     message=$2
 fi
 
+if [[ $# -eq 3 ]]; then
+    base_branch=$1
+    squash_branch=$2
+    message=$3
+fi
+
 echo "Squashing branch: $squash_branch"
 echo "Base branch:      $base_branch"
-echo "Checking out $squash_branch"
+
+echo "Checking out $squash_branch..."
 git checkout $squash_branch
+
 git reset $(git merge-base $base_branch $squash_branch)
 git add -A
 git commit -m "$message"
