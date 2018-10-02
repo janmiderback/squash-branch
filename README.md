da86cb7c4bce8b58c31aa94ea3e2222e4e0cc15c
Jan Miderbäck <jan.miderback@qlik.com>
2018-10-02 10:07:41 +0200

    Update

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # squash-branch
-Bash script to squash a branch into one commit using git merge-base
+
+Bash script to squash a Git branch into one commit using git merge-base.
+
+## Prerequisites
+
+- Git is installed
+- Bash environment is available
+
+On Windows, Git Bash provided with [Git for Windows](https://gitforwindows.org) is recommended.
+
+## Usage
+
+`squash-branch [base-branch] "commit-message"`
+
+If `base-branch` is not provided, `master` is used.
+
+### Examples
+
+To squash the current branch into one commit using `master` (default) as base branch:
+
+```bash
+squash-branch "My squashed commit"
+```
+
+To squash the current branch into one commit using `my-base-branch` as base branch:
+
+```bash
+squash-branch my-base-branch "My squashed commit"
+```
+
+To squash a the current branch using a multi-line commit message:
+
+```bash
+$ squash-branch "My squashed commit header
+> and some info on the second line
+> and some info on the third line"
+```
+
+## Install
+
+Copy the [squash-branch.sh](./squash-branch.sh) script to your local machine, preferrably so that it is available
+on your path.
+
+## Bash on Windows
+
+The script should work just fine in Git Bash provided with [Git for Windows](https://gitforwindows.org).
--- squash-branch.sh
+++ squash-branch.sh
@@ -1,35 +1,34 @@
 #!/usr/bin/env bash
-# Squash a branch to its merge base, and commit using provided commit message.
+# Squash current branch to its merge base, and commit using provided commit message.
+#
 # Usage:
-#   squash-branch [squash-branch] "commit-message"
-#   
+#   squash-branch [base-branch] "commit-message"
+#
+# If base-branch is not provided, master is used.
+#
+
 set -e
 
-if [[ $# -lt 1 || $# -gt 3 ]]; then
+if [[ $# -lt 1 || $# -gt 2 ]]; then
     echo "Error - Wrong number of arguments"
     exit 1
 fi
 
-# TODO: Make it possible to provide base branch as a first argument
-#       before squash-branch.
-base_branch=master
-squash_branch=
-commit_message=
+current_branch=$(git rev-parse --abbrev-ref HEAD)
 
 if [[ $# -eq 1 ]]; then
-    squash_branch=$(git rev-parse --abbrev-ref HEAD)
+    base_branch=master
     message=$1
 fi
 
 if [[ $# -eq 2 ]]; then
-    squash_branch=$1
+    base_branch=$1
     message=$2
 fi
 
-echo "Squashing branch: $squash_branch"
-echo "Base branch:      $base_branch"
-echo "Checking out $squash_branch"
-git checkout $squash_branch
-git reset $(git merge-base $base_branch $squash_branch)
+echo "Current branch: $current_branch"
+echo "Base branch:    $base_branch"
+
+git reset $(git merge-base $base_branch $current_branch)
 git add -A
 git commit -m "$message"
